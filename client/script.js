2d0e4d7dadd243160f4243c80ade9ad3ee6028d5
furkankaan <fkbicer07@gmail.com>
2024-08-02 14:42:22 +0300

    initial commit

---
+++ client/script.js
@@ -0,0 +1,33 @@
+import { io } from "socket.io-client"
+
+const joinRoomButton = document.getElementById("room-button")
+const messageInput = document.getElementById("message-input")
+const roomInput = document.getElementById("room-input")
+const form = document.getElementById("form")
+
+const socket = io('http://localhost:3000')
+
+socket.on("connect", () => {
+    displayMessage(`you connected with id: ${socket.id}`); // Template literal kullanarak dinamik id'yi ekleyin
+});
+
+form.addEventListener("submit", e => {
+    e.preventDefault()
+    const message = messageInput.value
+    const room = roomInput.value
+
+    if (message === "") return
+    displayMessage(message)
+
+    messageInput.value = ""
+})
+
+joinRoomButton.addEventListener("click", () => {
+    const room = roomInput.value
+})
+
+function displayMessage(message){
+    const div = document.createElement("div")
+    div.textContent = message
+    document.getElementById("message-container").append(div);
+}
