72bf1d35fcf6c0aa81da0ebbb2115f85224a5481
furkankaan <fkbicer07@gmail.com>
2024-08-04 23:54:18 +0300

    creating rest api's

--- php/api/v1/create_message.php
+++ php/api/v1/create_message.php
@@ -1,35 +1,35 @@
 <?php 
-require '../../functions/routing.php';
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type');
 
-$input = json_decode(file_get_contents('php://input'), true);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // POST verilerini al
+    $message = isset($_POST['message']) ? htmlspecialchars($_POST['message']) : '';
 
-if (isset($input['token'])
-    ) {
-    $token = $input['token'];
+    // Verilerin geçerli olup olmadığını kontrol et
+    if (!empty($message)) {
 
-    // Database connection
-    require '../../db/database.class.php';
-    $db = new Database();
-    $updateQuery= "UPDATE tokens SET expired_at = NOW() WHERE token = ?";
-    $updatedRowCount = $db->updateData($updateQuery,array($token));
-    if($updatedRowCount) {
-        http_response_code(201);
-        echo json_encode([
-            'message' => "Token terminated successfully.",
-            'token' => $token
-        ]);
-    }else {
-        http_response_code(500);
-        echo json_encode([
-            'message' => "Token cannot terminate."
-        ]);
+        // Database connection
+        require '../../db/database.class.php';
+        $db = new Database();
+        $insertQuery = 'INSERT INTO messages (content) VALUES (?)';
+        $userId = $db->insertData($insertQuery,array($message));
+
+        if($userId) {
+            http_response_code(201);
+            echo json_encode(['message' => 'User creation succesfull.']);
+        }else {
+            http_response_code(500);
+            echo json_encode(['message' => 'User creation failed.']);
+        }
+    } else {
+        // Hatalı veri durumunda yanıt döner
+        echo 'Invalid data';
     }
 } else {
-    echo "Bu sayfayı görüntülemeye yetkiniz bulunmamaktadır.";
-    go("login.html");
+    // Geçerli olmayan istek türü durumunda yanıt döner
+    echo 'Invalid request method';
 }
 ?>
