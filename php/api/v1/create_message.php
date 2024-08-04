183e391143f5620e698f3fbf9fcc4507df00e022
furkankaan <fkbicer07@gmail.com>
2024-08-05 00:14:19 +0300

    folder's path changing

--- php/api/v1/create_message.php
+++ php/api/v1/create_message.php
@@ -3,33 +3,27 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type');
+$input = json_decode(file_get_contents('php://input'), true);
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // POST verilerini al
-    $message = isset($_POST['message']) ? htmlspecialchars($_POST['message']) : '';
+if (isset($input['message'])
 
-    // Verilerin geçerli olup olmadığını kontrol et
-    if (!empty($message)) {
+    ) {
+    $message = $input['message'];
 
-        // Database connection
-        require '../../db/database.class.php';
-        $db = new Database();
-        $insertQuery = 'INSERT INTO messages (content) VALUES (?)';
-        $userId = $db->insertData($insertQuery,array($message));
-
-        if($userId) {
-            http_response_code(201);
-            echo json_encode(['message' => 'User creation succesfull.']);
-        }else {
-            http_response_code(500);
-            echo json_encode(['message' => 'User creation failed.']);
-        }
-    } else {
-        // Hatalı veri durumunda yanıt döner
-        echo 'Invalid data';
+    // Database connection
+    require '../../db/database.class.php';
+    $db = new Database();
+    $insertQuery = 'INSERT INTO messages (content) VALUES (?)';
+    $userId = $db->insertData($insertQuery,array($message));
+    if($userId) {
+        http_response_code(201);
+        echo json_encode(['message' => 'Message creation succesfull.']);
+    }else {
+        http_response_code(500);
+        echo json_encode(['message' => 'Message creation failed.']);
     }
 } else {
-    // Geçerli olmayan istek türü durumunda yanıt döner
-    echo 'Invalid request method';
+    http_response_code(400);
+    echo json_encode(['message' => 'Invalid input']);
 }
 ?>
